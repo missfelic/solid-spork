9e6a6800052c292dde1a12860173f2c7c750b25b
Felicity Beavan <felicity@Felicitys-MacBook-Pro.local>
2019-09-12 11:31:16 +0100

    Added:: Two random platforms Checks for collisions with the random platforms Destroy/Disable sprite falls off screen

--- my-app/js/PlayGame.js
+++ my-app/js/PlayGame.js
@@ -4,14 +4,14 @@ class PlayGame extends Phaser.Scene {
     }
     create(){
         // Loading Game
-        // Creating Titled Sprites
+        // Creating Titled Sprites  // DOESNT ANIMATE YET
         this.sky = this.add.tileSprite(0, 0, game.config.width, game.config.height, "sky");
         // Setting Pivot Point (Left Corner)
         this.sky.setOrigin(0, 0);
         // Fix Sky (So It Doesn't Move When Camera Moves)
         this.sky.setScrollFactor(0);
         
-        // Adding Second Background (Sides)
+        // Adding Second Background (Sides) // DOESNT ANIMATE YET
         this.sides = this.add.tileSprite(0, 0, 0, 0, "sides");
         // Setting Pivot Point (Left Corner)
         this.sides.setOrigin(0, 0);
@@ -26,8 +26,14 @@ class PlayGame extends Phaser.Scene {
         // Add Starting Ground
 
         // Starting Platform
-        this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(this.game.config.width / 2, game.config.height / 1.2, 'platforms').setScale(0.2).refreshBody();
+        this.startPlatform = this.physics.add.staticGroup();
+        this.startPlatform.create(this.game.config.width / 2, game.config.height / 1.2, 'log').setScale(0.2).refreshBody();
+
+        // Endless Platforms
+        this.endlessPlatforms = this.physics.add.staticGroup();
+        this.endlessPlatforms.create(randX(), randY(), 'platforms').setScale(0.2).refreshBody();
+        this.endlessPlatforms.create(randX(), randY(), 'log').setScale(0.2).refreshBody();
+
 
         // Add pinkMonster
         this.pinkMonster = this.physics.add.sprite(game.config.width / 2, game.config.height / 1.2, 'pinkMonster');
@@ -47,25 +53,24 @@ class PlayGame extends Phaser.Scene {
         // Making The Camera Follow The pinkMonster
         // this.myCam.startFollow(this.pinkMonster);
         
-        // Adding Collision
-        this.physics.add.collider(this.pinkMonster, this.platforms);
+        // Adding Collision With Starting Platform
+        this.physics.add.collider(this.pinkMonster, this.startPlatform);
+        // Adding Collision With Endless Platforms
+        this.physics.add.collider(this.pinkMonster, this.endlessPlatforms);
 
         // Key Inputs To Control The pinkMonster
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        ////////// NEED TO RE THINK ///////////
-        let randX = Math.floor(Math.random() * game.config.width);
-        let randY = Math.floor(Math.random() * game.config.height);
-        console.log('Height::',game.config.height, 'Width::', game.config.width)
-        console.log('X::', randX, 'Y::', randY);
-
     }
 
+ 
 
 
     
 
     update() {
+        // checkX(this.endlessPlatforms)
+        // checkY(this.endlessPlatforms)
 
         // Keyboard Inputs
         if (this.cursors.left.isDown) {
@@ -79,11 +84,18 @@ class PlayGame extends Phaser.Scene {
         }
 
         // Check For Collision With Platform
-        if (this.physics.add.collider(this.pinkMonster, this.platforms)){
+        if (this.physics.add.collider(this.pinkMonster)){
             // If Collision - Y ( Bounce )
             this.pinkMonster.y -= 10;
         }
 
+        // if (this.pinkMonster.y == this.log.y || this.pinkMonster.y == this.platforms.y)
+
+         // Destroy pinkMonster If pinkMoster Falls Off Screen
+         if(this.pinkMonster.y > game.config.height) {
+             this.pinkMonster.disableBody(true, true)
+             console.log('Sprite DESTROYED::', this.pinkMonster, 'Disabled body::', this.pinkMonster.disableBody);
+         }
         // Scroll The Background
         // // Scroll The Texture Of The TileSprites 
         // this.sky.titlePositionY = this.myCam.scrollY * .3;
