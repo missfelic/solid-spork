582e685e77e0b3cfa4381c4f215176ba0e91b4f9
Felicity Beavan <felicity@Felicitys-MacBook-Pro.local>
2019-09-19 14:14:36 +0100

    Adding back randomX & randomY functions

---
+++ my-app/js/utlis/utlis.js
@@ -0,0 +1,16 @@
+//    **    //
+ // Utilities // 
+//    **   //
+
+// Random X Between Width Of My Game
+function randomX () {
+    let randomX = Math.floor(Math.random() * ( 250 - 100) + 100);
+    return randomX;
+}
+
+// Random Y Between Height Of My Game
+function randomY () {
+    let randomY = Math.floor(Math.random() * (700 - 100) + 100);
+    return randomY;
+}
+
