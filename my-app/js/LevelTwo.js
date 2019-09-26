5d38176840828d78062c292444e60b29cf762f40
Felicity Beavan <felicity@Felicitys-MacBook-Pro.local>
2019-09-26 13:02:52 +0100

    LevelTwo

---
+++ my-app/js/LevelTwo.js
@@ -0,0 +1,176 @@
+class LevelTwo extends Phaser.Scene {
+  constructor() {
+    super("LevelTwo");
+  }
+
+  create() {
+    // Load Sounds
+    this.sfx = {
+      // bgSound: this.sound.add("background"),
+      jumpSound: this.sound.add("jump"),
+      coinSound: this.sound.add("coinSound")
+    };
+    // Play Background Music
+    // this.sfx.bgSound.play();
+
+    // Loading Game
+    // Adding First Background Sky
+    this.sky = this.add.tileSprite(
+      0,
+      0,
+      game.config.width,
+      game.config.height,
+      "sky"
+    );
+    // Setting Pivot Point (Left Corner)
+    this.sky.setOrigin(0, 0);
+
+    // Adding Second Background (Sides)
+    this.sides = this.add.tileSprite(0, 0, 0, 0, "sides");
+    // Setting Pivot Point (Left Corner)
+    this.sides.setOrigin(0, 0);
+    // Setting Scale
+    this.sides.setScale(0.2, 0.2);
+
+    // Add pinkMonster
+    this.pinkMonster = this.physics.add.sprite(
+      game.config.width / 2,
+      game.config.height / 1.2,
+      "pinkMonster"
+    );
+    // Create pinkMonster Animation (Idle)
+    this.anims.create({
+      key: "idle",
+      frames: this.anims.generateFrameNumbers("pinkMonster"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.pinkMonster.play("idle");
+
+    const defaultPosY = this.game.config.height / 1.1;
+    const defaultPosX = this.game.config.width / 2;
+
+    // Starting Platform
+    this.startPlatform = this.physics.add.staticGroup();
+    this.startPlatform
+      .create(defaultPosX, defaultPosY, "grass")
+      .setScale(0.2)
+      .refreshBody();
+
+    // Creating Random Platforms Group
+    this.platforms = this.physics.add.staticGroup();
+    // Adding all the platforms
+    this.platforms
+      .create(190, 600, "log")
+      .setScale(0.2)
+      .refreshBody();
+    this.platforms
+      .create(120, 500, "grass")
+      .setScale(0.2)
+      .refreshBody();
+    this.platforms
+      .create(100, 365, "log")
+      .setScale(0.2)
+      .refreshBody();
+    this.platforms
+      .create(220, 265, "log")
+      .setScale(0.2)
+      .refreshBody();
+    this.platforms
+      .create(150, 145, "log")
+      .setScale(0.2)
+      .refreshBody();
+    this.platforms
+      .create(100, 70, "grass")
+      .setScale(0.2)
+      .refreshBody();
+
+    // this.coins = this.physics.add.group();
+
+    // Add Special Coin
+    this.coin = this.physics.add
+      .sprite(randomX(), randomY(), "coins")
+      .setScale(0.12);
+    // Creating Animation
+    this.anims.create({
+      key: "spin",
+      frames: this.anims.generateFrameNumbers("coins"),
+      frameRate: 8,
+      repeat: -1
+    });
+    this.coin.body.allowGravity = false;
+    this.coin.play("spin");
+
+    // Create coin group
+    // push coins to the group
+    // check for collisions with player & platforms
+    // if collision respawn the coin
+    // make sure there are 10 on the screen
+
+    // Adding Collision With Starting Platform
+    this.physics.add.collider(this.pinkMonster, this.startPlatform);
+
+    // Adding Collision With Platforms
+    this.physics.add.collider(this.pinkMonster, this.platforms);
+
+    // Adding Collision With Platforms
+    this.physics.add.collider(this.coin, this.platforms);
+
+    // Key Inputs To Control The pinkMonster
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Checking for overlap
+    this.physics.add.overlap(this.pinkMonster, this.coin, this.collectStar);
+
+    // Score
+    score = 0;
+
+    scoreText = this.add.text(0, 0, "Score: " + score, {
+      font: "20px Arial",
+      fill: "#000"
+    });
+  }
+
+  // Collect function
+  collectStar = (pinkMonster, coin) => {
+    this.sfx.coinSound.play();
+    coin.destroy();
+    score += 25;
+    scoreText.setText("Score: " + score);
+  };
+
+  update() {
+    // Keyboard Inputs
+    // Left & Right Controls
+    if (this.cursors.left.isDown) {
+      this.pinkMonster.setVelocityX(-120);
+      this.pinkMonster.anims.play("idle", true);
+      this.pinkMonster.flipX = true;
+    } else if (this.cursors.right.isDown) {
+      this.pinkMonster.setVelocityX(120);
+      this.pinkMonster.anims.play("idle", true);
+      this.pinkMonster.flipX = false;
+    }
+
+    // Jumping Controls
+    if (this.cursors.space.isDown && this.pinkMonster.body.touching.down) {
+      this.pinkMonster.setVelocityY(-500);
+      // Adding Jump sound
+      this.sfx.jumpSound.play();
+    }
+
+    // If you fall then
+    if (this.pinkMonster.y > game.config.height) {
+      // Loads Level One
+      game.scene.start("PlayGame");
+      game.scene.stop("LevelTwo");
+    }
+
+    // If you reach the top then
+    if (this.pinkMonster.y <= 0) {
+      // Loads Level Three
+      game.scene.start("LevelThree");
+      game.scene.stop("LevelTwo");
+    }
+  }
+}
