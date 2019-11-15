e66af4148b2915768d6c506b41b83513880a1ccd
Felicity Beavan <felicity@Felicitys-MacBook-Pro.local>
2019-11-15 22:12:53 +0000

    Removed Unused files

--- my-app/js/Player.js
+++
@@ -1,8 +0,0 @@
-class Player extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene) {
-    const x = game.config.width / 2;
-    const y = game.config.height / 1.2;
-
-    super(scene, x, y, "pinkMonster");
-  }
-}
