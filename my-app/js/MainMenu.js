bd26da3f0068253ab21819b8ae23a9adfbb1452d
Felicity Beavan <felicity@Felicitys-MacBook-Pro.local>
2019-11-21 11:03:37 +0000

    Removed unused things

--- my-app/js/MainMenu.js
+++ my-app/js/MainMenu.js
@@ -12,46 +12,19 @@ class MainMenu extends Phaser.Scene {
     );
     this.titlescreen.setScale(0.2);
 
-    // TODO::
-    //   // Loading bar
-    //   this.loading = this.add.sprite(
-    //     game.config.width / 2,
-    //     game.config.height / 2,
-    //     "loading"
-    //   );
-
-    // this.loading.setScale(0.5);
-
     // Play
-    playText = this.add.text(125, 350, "Play", {
-      font: "20px ",
-      fill: "#000"
-    });
-    // Stages
-    stagesText = this.add.text(125, game.config.height / 2, "Stages", {
-      font: "20px ",
-      fill: "#000"
-    });
-    // Options
-    optionsText = this.add.text(125, 450, "Options", {
+    playText = this.add.text(125, 350, "Play!", {
       font: "20px ",
       fill: "#000"
     });
 
     // Making Text Interactive
     playText.setInteractive({ useHandCursor: true });
-    optionsText.setInteractive({ useHandCursor: true });
 
     // Load Game
     playText.on("pointerdown", () => {
       game.scene.stop("MainMenu");
       game.scene.start("LevelOne");
-    });
-
-    // Load Options
-    optionsText.on("pointerdown", () => {
-      game.scene.stop("MainMenu");
-      game.scene.start("Options");
     });
   }
 }
